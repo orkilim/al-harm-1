90449df056387501a6d5d88a0ccbba2e821feccc
or kilim <48524233+orkilim@users.noreply.github.com>
2021-05-22 20:00:15 +0300

    can now record and buttons disappear and reappear

--- Components/EndSession.js
+++ Components/EndSession.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ActivityIndicator, TouchableOpacity, PermissionsAndroid,NativeModules, NativeEventEmitter, StyleSheet } from 'react-native';
+import { Button, View, Text, ActivityIndicator, TouchableOpacity, PermissionsAndroid, NativeModules, NativeEventEmitter, StyleSheet } from 'react-native';
 import BleManager, { stopScan } from 'react-native-ble-manager';
 import axios from 'axios'
 import BackgroundTimer from 'react-native-background-timer';
 import Geolocation from 'react-native-geolocation-service';
 import Geocoder from 'react-native-geocoding';
+import SoundRecorder from 'react-native-sound-recorder';
+import PushNotification from 'react-native-push-notification';
+import SoundPlayer from 'react-native-sound-player';
 
 const BleManagerModule = NativeModules.BleManager
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule)
-let connected=false
+let connected = false
 let scanInterval
+let pathToRecording
+//const recordingsArray = []
+//const recordingCounter = 0
+let recordingInterval
 
 export default function EndSession({ route, navigation }) {
   const { contacts, woman, token } = route.params
   const [show, setShow] = useState(false)
-  const [isScanning, setIsScanning] = useState(false)
+  const [isScanning, setIsScanning] = useState(false)//are we scanning for al-harm ble devices
   const peripherals = new Map()
   const [list, setList] = useState([])
-  const [connectionToUser, setConnection] = useState("not connected to a ble device")
+  const [connectionToUser, setConnection] = useState("not connected to a ble device")//will show whether we are connected to a user or not
+  const [isRecording, setIsRecording] = useState("")//shows if we are recording or not
+  const [stopRecordingButtonVisibility, setVisibility] = useState(false)
+  const [playVisibility, setPlayVisibility] = useState(false)
 
   const switchShow = () => setShow(show => !show)
 
@@ -45,11 +55,12 @@ export default function EndSession({ route, navigation }) {
     saveData();
   }, [])
 
+
+
   useEffect(() => {
 
     //ask for permission to activate ble and gps
     askForPermission()
-
 
     //initiate ble
     BleManager.start({ showAlert: false });
@@ -60,8 +71,6 @@ export default function EndSession({ route, navigation }) {
     bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', handleUpdateValueForCharacteristic);
 
     scanAndConnect()
-
-
   }, [])
 
 
@@ -69,14 +78,14 @@ export default function EndSession({ route, navigation }) {
 
   //function to scan for ble al-harm devices and connect with them
   const scanAndConnect = () => {
-      BleManager.scan([], 10, false)
-        .then((peripheral) => {
-          BleManager.getDiscoveredPeripherals()
-        })
-        .catch((err) => {
-          if (err)
-            console.log("error in discovering peripherals: " + err)
-        })
+    BleManager.scan([], 10, false)
+      .then((peripheral) => {
+        BleManager.getDiscoveredPeripherals()
+      })
+      .catch((err) => {
+        if (err)
+          console.log("error in discovering peripherals: " + err)
+      })
   }
 
 
@@ -138,72 +147,89 @@ export default function EndSession({ route, navigation }) {
       setList(Array.from(peripherals.values()));
       BleManager.stopScan()
       peripheralConnectedTo = peripheral.id
-      scanInterval=BackgroundTimer.setInterval(() => {
-        if(!connected)
-        {
-        console.log("connecting the user to the al-harm device")
-        BleManager.connect(peripheral.id)
-          .then(async () => {
-            console.log("we are connected as a user")
-            connected=true
-            await BleManager.retrieveServices(peripheral.id)
-              .then(async (peripheralInfo) => {
-                while (BleManager.isPeripheralConnected()) {
-                  setConnection("you are connected to a user")
-                  await BleManager.read(peripheral.id, "12345678-0000-1000-8000-12345678ABCD", "87654321-0000-1000-8000-DCBA87654321")
-                    .then(async (data) => {
-                      console.log("my data: " + data[0])
-                      if (data[0] == 90) {
+      scanInterval = BackgroundTimer.setInterval(() => {
+        if (!connected) {
+          console.log("connecting the user to the al-harm device")
+          BleManager.connect(peripheral.id)
+            .then(async () => {
+              console.log("we are connected as a user")
+              connected = true
+              await BleManager.retrieveServices(peripheral.id)
+                .then(async (peripheralInfo) => {
+                  while (BleManager.isPeripheralConnected()) {
+                    setConnection("you are connected to a user")
+                    await BleManager.read(peripheral.id, "12345678-0000-1000-8000-12345678ABCD", "87654321-0000-1000-8000-DCBA87654321")
+                      .then(async (data) => {
+                        console.log("my data: " + data[0])
+                        if (data[0] == 90) {
 
-                        /**here we find the address and we send the SOS signal */
+                          /**here we find the address and we send the SOS signal */
 
-                        //----we need to add a call to the al-harm server to LOG the SOS sent----
-
+                          //----we need to add a call to the al-harm server to LOG the SOS sent----
 
 
-                        Geolocation.getCurrentPosition(
-                          (position) => {
-                            /**we take the received coordinates and turn them to an address */
-                            Geocoder.init("AIzaSyBm3MC9Zv5sYVb23j8zkJWnSM2p12VXlM0")//uses the google API key- we need to hide it
-                            Geocoder.from({ lat: position.coords.latitude, lng: position.coords.longitude })
-                              .then(json => {
-                                json.results[0]
-                                const addressComponent = json.results[0].formatted_address;
-                                console.log(addressComponent);
-                              })
-                              .catch(error => console.warn("error in geocoding from coordinates to address: " + error));
 
-                          },
-                          (error) => {
-                            // See error code charts below.
-                            console.log("error in finding coordinates: " + error.code, error.message);
-                          },
-                          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-                        );
+                          Geolocation.getCurrentPosition(
+                            (position) => {
+                              /**we take the received coordinates and turn them to an address */
+                              Geocoder.init("AIzaSyBm3MC9Zv5sYVb23j8zkJWnSM2p12VXlM0")//uses the google API key- we need to hide it
+                              Geocoder.from({ lat: position.coords.latitude, lng: position.coords.longitude })
+                                .then(json => {
+                                  json.results[0]
+                                  const addressComponent = json.results[0].formatted_address;
+                                  console.log(addressComponent);
+                                })
+                                .catch(error => console.warn("error in geocoding from coordinates to address: " + error));
 
-                        /**sending the SOS signal to the close friends/remote guardians*/
-                        //const msg = data[0].toString()
-                        twilio()
-                      }
+                            },
+                            (error) => {
+                              // See error code charts below.
+                              console.log("error in finding coordinates: " + error.code, error.message);
+                            },
+                            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+                          );
 
-                    })
-                    .catch((error) => {
-                      if (error)
-                        console.log("my error: " + error)
-                    })
-                }
-              })
-              .catch((innerErr) => {
-                if (innerErr)
-                  console.log("innerErr: " + innerErr)
-              })
-          })
-          .catch((err) => {
-            if (err)
-              console.log("a problem with connection to ble:\n" + err)
-          })
+                          /**sending the SOS signal to the close friends/remote guardians*/
+                          //const msg = data[0].toString()
+                          twilio()
+
+                          /**starting a new recording and a notification interval to remind that it is recording */
+
+                          SoundRecorder.start(SoundRecorder.PATH_CACHE + "/al_harm_recording.mp4")
+                            .then(function () {
+                              console.log('started recording');
+                              //recordingCounter++;
+                              setIsRecording("recording...")
+                              recordingInterval = BackgroundTimer.setInterval(() => {
+                                PushNotification.localNotification({
+                                  channelId: "1",
+                                  title: "al-harm still recording...",
+                                  message: "close the app completely or hit the STOP button in-app to stop recording"
+                                })
+                              }, 30000)
+                              setVisibility(true)
+                            });
+
+                        }
+
+                      })
+                      .catch((error) => {
+                        if (error)
+                          console.log("my error: " + error)
+                      })
+                  }
+                })
+                .catch((innerErr) => {
+                  if (innerErr)
+                    console.log("innerErr: " + innerErr)
+                })
+            })
+            .catch((err) => {
+              if (err)
+                console.log("a problem with connection to ble:\n" + err)
+            })
         }
-      },1000)
+      }, 1000)
       BackgroundTimer.clearInterval()
     }
   }
@@ -230,7 +256,24 @@ export default function EndSession({ route, navigation }) {
     }
   }
 
-  const Button = ({ text, onPress }) => {
+  const stopRec = () => {
+    SoundRecorder.stop()
+      .then(function (result) {
+        setIsRecording("stopped recording")
+        console.log('stopped recording, audio file saved at: ' + result.path);
+        BackgroundTimer.clearInterval(recordingInterval)
+        pathToRecording = result.path
+        setVisibility(false)
+        setPlayVisibility(true)
+      });
+  }
+
+  const playRec = () => {
+    SoundPlayer.playUrl(pathToRecording)
+  }
+
+  //-------possible rendering options for the EndSession component-----
+  /*const Button = ({ text, onPress }) => {
     return (
       <TouchableOpacity onPress={onPress}>
         <View style={styles.btnContainerStyle}>
@@ -238,18 +281,27 @@ export default function EndSession({ route, navigation }) {
         </View>
       </TouchableOpacity>
     )
-  }
+  }*/
+
+  /*<Button
+            text="sms"
+            onPress={() => twilio()}
+          />*/
 
   const textToUser = () => {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={{ fontSize: 40, marginTop: -20, textAlign: 'center' }}>Data Saved Successully!</Text>
         <View style={styles.fixToText}>
-          <Button
-            text="sms"
-            onPress={() => twilio()}
-          />
+          <Button title="send sms to contacts" onPress={() => { twilio() }} ></Button>
           <Text>{connectionToUser}</Text>
+          <View style={{ display: stopRecordingButtonVisibility ? "flex" : "none" }} >
+            <Button title="stop recording" onPress={() => { stopRec() }}></Button>
+          </View>
+          <Text>{isRecording}</Text>
+          <View style={{ display: playVisibility ? "flex" : "none" }} >
+            <Button title="play recording" onPress={() => { playRec() }} ></Button>
+          </View>
         </View>
       </View>
     )
